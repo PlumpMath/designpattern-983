95a6d1a952d0d36a1ee49791d26135f2437b06ea
Sungkuk Park <sungkukpark@maverickgames.com>
2016-12-12 21:41:39 +0900

    Mv introduction and duck classes under the root namespace

--- HeadFirstDesignPattern/HeadFirstDesignPattern/Introduction.cs
+++ HeadFirstDesignPattern/HeadFirstDesignPattern/Introduction.cs
@@ -2,135 +2,138 @@
 
 using System;
 
-static class Logger
+namespace HeadFirstDesignPattern
 {
-    public static void Log(string text)
+    static class Logger
     {
-        Console.WriteLine(text);
-    }
-}
-
-// 오리
-class Duck
-{
-    public virtual void quack()
-    {
-        Logger.Log("꽦꽦");
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
     }
 
-    public virtual void swim()
+    // 오리
+    class Duck
     {
-        Logger.Log("헤엄헤엄");
+        public virtual void quack()
+        {
+            Logger.Log("꽦꽦");
+        }
+
+        public virtual void swim()
+        {
+            Logger.Log("헤엄헤엄");
+        }
+
+        public virtual void fly()
+        {
+            Logger.Log("파닥파닥");
+        }
+
+        public virtual void display()
+        {
+            Logger.Log("오리모양");
+        }
     }
 
-    public virtual void fly()
+    // 청둥오리
+    class MallardDUck : Duck
     {
-        Logger.Log("파닥파닥");
+        public override void display()
+        {
+            Logger.Log("청둥오리모양");
+        }
     }
 
-    public virtual void display()
-    {
-        Logger.Log("오리모양");
-    }
-}
 
-// 청둥오리
-class MallardDUck : Duck
-{
-    public override void display()
+    // 아메리카 흰죽지
+    class RedHeadDuck : Duck
     {
-        Logger.Log("청둥오리모양");
-    }
-}
-
-
-// 아메리카 흰죽지
-class RedHeadDuck : Duck
-{
-    public override void display()
-    {
-        Logger.Log("아메리카흰죽지모양");
-    }
-}
-
-// 고무오리
-class RubberDuck : Duck
-{
-    public override void quack()
-    {
-        Logger.Log("쁶쁶");
+        public override void display()
+        {
+            Logger.Log("아메리카흰죽지모양");
+        }
     }
 
-    public override void display()
+    // 고무오리
+    class RubberDuck : Duck
     {
-        Logger.Log("고무오리모양");
+        public override void quack()
+        {
+            Logger.Log("쁶쁶");
+        }
+
+        public override void display()
+        {
+            Logger.Log("고무오리모양");
+        }
+
+        public override void fly()
+        {
+            // do nothing
+        }
     }
 
-    public override void fly()
+    // 나무오리
+    class DecoyDuck : Duck
     {
-        // do nothing
+        public override void quack()
+        {
+            // do nothing
+        }
+
+        public override void display()
+        {
+            Logger.Log("나무오리모양");
+        }
+
+        public override void fly()
+        {
+            // do nothing
+        }
     }
-}
 
-// 나무오리
-class DecoyDuck : Duck
-{
-    public override void quack()
+    class Introduction
     {
-        // do nothing
-    }
+        static void Main(string[] args)
+        {
+            Duck duck = new Duck();
+            duck.quack();
+            duck.swim();
+            duck.display();
+            duck.fly();
 
-    public override void display()
-    {
-        Logger.Log("나무오리모양");
-    }
+            Logger.Log("\n");
 
-    public override void fly()
-    {
-        // do nothing
-    }
-}
+            MallardDUck mallardDuck = new MallardDUck();
+            mallardDuck.quack();
+            mallardDuck.swim();
+            mallardDuck.display();
+            mallardDuck.fly();
 
-class Introduction
-{
-    static void Main(string[] args)
-    {
-        Duck duck = new Duck();
-        duck.quack();
-        duck.swim();
-        duck.display();
-        duck.fly();
+            Logger.Log("\n");
 
-        Logger.Log("\n");
+            RedHeadDuck redheadDuck = new RedHeadDuck();
+            redheadDuck.quack();
+            redheadDuck.swim();
+            redheadDuck.display();
+            redheadDuck.fly();
 
-        MallardDUck mallardDuck = new MallardDUck();
-        mallardDuck.quack();
-        mallardDuck.swim();
-        mallardDuck.display();
-        mallardDuck.fly();
+            Logger.Log("\n");
 
-        Logger.Log("\n");
+            RubberDuck rubberDuck = new RubberDuck();
+            rubberDuck.quack();
+            rubberDuck.swim();
+            rubberDuck.display();
+            rubberDuck.fly();
 
-        RedHeadDuck redheadDuck = new RedHeadDuck();
-        redheadDuck.quack();
-        redheadDuck.swim();
-        redheadDuck.display();
-        redheadDuck.fly();
+            Logger.Log("\n");
 
-        Logger.Log("\n");
-
-        RubberDuck rubberDuck = new RubberDuck();
-        rubberDuck.quack();
-        rubberDuck.swim();
-        rubberDuck.display();
-        rubberDuck.fly();
-
-        Logger.Log("\n");
-
-        DecoyDuck decoyDuck = new DecoyDuck();
-        decoyDuck.quack();
-        decoyDuck.swim();
-        decoyDuck.display();
-        decoyDuck.fly();
+            DecoyDuck decoyDuck = new DecoyDuck();
+            decoyDuck.quack();
+            decoyDuck.swim();
+            decoyDuck.display();
+            decoyDuck.fly();
+        }
     }
 }
