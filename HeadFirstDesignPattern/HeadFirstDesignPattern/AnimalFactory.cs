2233abd5b942b8cea705111e970f29f6e43dba47
Sungkuk Park <sungkukpark@maverickgames.com>
2016-12-12 21:57:30 +0900

    Add animal factory

---
+++ HeadFirstDesignPattern/HeadFirstDesignPattern/AnimalFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPattern
+{
+    class AnimalFactory
+    {
+        List<Animal> animalList;
+
+        List<Animal> getAnimals()
+        {
+            if (animalList != null)
+                return animalList;
+
+            animalList = new List<Animal>();
+            animalList.Add(new Dog());
+            animalList.Add(new Cat());
+            return animalList;
+        }
+    }
+}
