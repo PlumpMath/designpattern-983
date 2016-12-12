12b7761563d7ec2ae8e2366ccb20ed05a2391913
Sungkuk Park <sungkukpark@maverickgames.com>
2016-12-12 20:29:34 +0900

    Add introduction class

---
+++ HeadFirstDesignPattern/HeadFirstDesignPattern/Introduction.cs
@@ -0,0 +1,21 @@
+﻿// <<Head First Design Patterns> p.40
+
+using System;
+
+class Duck
+{
+    // "꽥꽥"하고 운다
+    public void quack()
+    {
+        Console.WriteLine("꽥꽥!");
+    }
+}
+
+class Introduction
+{
+    static void Main(string[] args)
+    {
+        Duck duck = new Duck();
+        duck.quack();
+    }
+}
