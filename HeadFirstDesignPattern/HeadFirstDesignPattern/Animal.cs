1efc828c8fa230cc8abaa4c747069057da1c5e22
Sungkuk Park <sungkukpark@maverickgames.com>
2016-12-12 21:36:27 +0900

    Add animal class

---
+++ HeadFirstDesignPattern/HeadFirstDesignPattern/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HeadFirstDesignPattern
+{
+    interface MakeSoundBehavior
+    {
+        void makeSound();
+    }
+
+    class Animal : MakeSoundBehavior
+    {
+        public virtual void makeSound()
+        {
+        }
+    }
+}
