d423e8a9500eadd976901a722fd58ded67bd45d4
Sungkuk Park <sungkukpark@maverickgames.com>
2016-12-24 10:32:14 +0900

    Add sample implementions

--- HeadFirstDesignPattern/ObserverPattern/Program.cs
+++ HeadFirstDesignPattern/ObserverPattern/Program.cs
@@ -6,27 +6,82 @@ using System.Threading.Tasks;
 
 static class Logger
 {
-    public static void Log(string text)
+    public static void Log(string format, object arg0, object arg1, object arg2)
     {
-        Console.WriteLine(text);
+        Console.WriteLine(format, arg0, arg1, arg2);
     }
 }
 
 namespace ObserverPattern
 {
     // Bridge object between "Weather Station" and "Weather Display"
-    public abstract class WeatherData
+    public class WeatherData
     {
-        public abstract float getTemperature();
-        public abstract float getHumidity();
-        public abstract float getPressure();
+        public WeatherData()
+        {
+            conditionDisplay = new ConditionDisplay();
+            statisticsDisplay = new StatisticsDisplay();
+            forecastDisplay = new ForecastDisplay();
+        }
+
+        public ConditionDisplay conditionDisplay;
+        public StatisticsDisplay statisticsDisplay;
+        public ForecastDisplay forecastDisplay;
+
+        public float getTemperature()
+        {
+            return 62f;
+        }
+        public float getHumidity()
+        {
+            return 50f;
+        }
+        public float getPressure()
+        {
+            return 78f;
+        }
         /*
          * 기상 관측값이 갱신될 때마다
          * 알려주기 위한 메소드
          */
         public void measurementsChanged()
         {
-            // Implement here
+            var temp = getTemperature();
+            var humidity = getHumidity();
+            var pressure = getPressure();
+
+            conditionDisplay.Update(temp, humidity, pressure);
+            statisticsDisplay.Update(temp, humidity, pressure);
+            forecastDisplay.Update(temp, humidity, pressure);
+        }
+    }
+
+    interface WeatherDisplay
+    {
+        void Update(float temp, float humidity, float pressure);
+    }
+
+    public class ConditionDisplay : WeatherDisplay
+    {
+        public void Update(float temp, float humidity, float pressure)
+        {
+            Logger.Log("Display 현재 조건: {0}, {1}, {2}", temp, humidity, pressure);
+        }
+    }
+
+    public class StatisticsDisplay : WeatherDisplay
+    {
+        public void Update(float temp, float humidity, float pressure)
+        {
+            Logger.Log("Display 기상 통계: {0}, {1}, {2}", temp, humidity, pressure);
+        }
+    }
+
+    public class ForecastDisplay : WeatherDisplay
+    {
+        public void Update(float temp, float humidity, float pressure)
+        {
+            Logger.Log("Display 기상 예측: {0}, {1}, {2}", temp, humidity, pressure);
         }
     }
 
@@ -34,7 +89,8 @@ namespace ObserverPattern
     {
         static void Main(string[] args)
         {
-
+            var wd = new WeatherData();
+            wd.measurementsChanged();
         }
     }
 }
